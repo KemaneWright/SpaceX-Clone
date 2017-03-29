07c4deea6ddfc1a53c8f846179c0608535876b48
Kemane Wright <Kemane.Wright25@gmail.com>
2017-03-29 14:47:22 -0600

    navbar

---
+++ js/spaceXApp.js
@@ -0,0 +1,31 @@
+angular.module('spaceXApp', ['ui.router'])
+.config (function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: './views/home.html'
+    })
+    .state('falcon9', {
+      url: '/falcon9',
+      templateUrl: './views/falcon9.html'
+    })
+    .state('falcon-heavy', {
+      url: '/falcon-heavy',
+      templateUrl: './views/falcon-heavy.html'
+    })
+    .state('dragon', {
+      url: '/dragon',
+      templateUrl: './views/dragon.html'
+    })
+    .state('updates', {
+      url: '/updates',
+      templateUrl: './views/updates.html'
+    })
+
+  $urlRouterProvider
+  .otherwise('/')
+
+
+
+
+})
